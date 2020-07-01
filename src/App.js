1ccd55c219b5b7adf057e9297a9c2e1fc56bf84b
sxena <s_xena@hotmail.com>
2020-07-01 11:03:15 -0700

    what is going on

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './App.css'
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      // Add magic 8 ball outcomes to the array
+      answerArray: ['yes', 'no', 'maybe'],
+      question: ""
+    }
+  }
+
+  getAnswer = () => {
+    // destructures answerArray out of state
+    const { answerArray } = this.state
+        let answerRandom = Math.floor(Math.random() * 3);
+    return answerArray[answerRandom]
+
+
+
+    // write your code here!
+    // Update the return statement below to reflect the outcome of your code.
+
+  }
+
+  handleChange = (e) => {
+    // gets the event from the input on change and updates state
+    this.setState({ question: e.target.value })
+  }
+
+  handleSubmit = () =>{
+    // destructures question out of state
+    const { question } = this.state
+    // calls the getAnswer function and saves the outcome as answer
+    const answer = this.getAnswer()
+    this.setState({ answer: answer })
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Magic 8 Ball</h1>
+        <input
+          id="inputBox"
+          type='text'
+          onChange={ this.handleChange }
+        />
+        <br />
+        <button onClick={ this.handleSubmit }>
+          Ask the Magic 8 Ball a Question
+        </button>
+        <p id = "ball8"> { this.state.answer } </p>
+      </div>
+    )
+  }
+}
+
+export default App;
